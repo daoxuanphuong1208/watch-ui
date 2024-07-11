4356fa21723b8f5e1f456313f145fabe6cd28c14
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2024-07-11 12:45:37 +0700

    code ContentItem

--- src/components/ContentItem/ContentItem.js
+++ src/components/ContentItem/ContentItem.js
@@ -3,8 +3,20 @@ import classNames from 'classnames/bind';
 import style from './ContentItem.module.scss';
 
 const cx = classNames.bind(style);
-function ContentItem() {
-    return <div className={cx('wrapper')}></div>;
+function ContentItem({ content }) {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('title')}>
+                    <h2 className={cx('large')}>Đối tác</h2>
+                    <p className={cx('description')}>
+                        Bộ sưu tập những thương hiệu nổi tiếng về smartwatch trên thế giới
+                    </p>
+                </div>
+                <div>{content}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ContentItem;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,14 @@ import styles from './Home.module.scss';
 // eslint-disable-next-line no-unused-vars
 import images from '../../assets/images';
 import SimpleSlider from '../../components/Slider/Slider';
+import ContentItem from '../../components/ContentItem/ContentItem';
 
 const cx = classNames.bind(styles);
 function Home() {
     return (
         <div className={cx('wrapper')}>
             <SimpleSlider />
+            <ContentItem />
         </div>
     );
 }
